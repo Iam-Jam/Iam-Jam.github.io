84482f642c966bd5bf5718e10dcfccc10fb10209
Iam-Jam <jammietorayno@gail.com>
2024-03-26 19:06:48 +0800

    2.25 assign

---
+++ JS-Crash-Course/3.25/assignment/3.25-assign.js
@@ -0,0 +1,18 @@
+let num1 = prompt("Enter Minimum Number");
+let num2 = prompt("Enter Maximum Number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+function printEvenNumbers(start, end) {
+    if (start > end) {20
+        return;
+    }
+
+    if (start % 2 === 0) {
+        console.log(start);
+    }
+    printEvenNumbers(start + 1, end);
+}
+
+printEvenNumbers(num1, num2);
