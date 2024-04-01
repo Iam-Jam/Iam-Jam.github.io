3daf33f826cd319ce9a34e4cf4723a0a0b0d4488
Iam-Jam <jammietorayno@gail.com>
2024-04-01 08:36:12 -0400

    dom

---
+++ JS-Crash-Course/4.1/dom.js
@@ -0,0 +1,34 @@
+document.getElementById("btn-primary").addEventListener("click", function() {
+    document.getElementById("div-body").style.backgroundColor = "#0275d8";
+});
+
+document.getElementById("btn-info").addEventListener("click", function() {
+    document.getElementById("div-body").style.backgroundColor = "#5bc0de";
+});
+
+document.getElementById("btn-danger").addEventListener("click", function() {
+    document.getElementById("div-body").style.backgroundColor = "#d9534f";
+});
+
+document.getElementById("btn-success").addEventListener("click", function() {
+    document.getElementById("div-body").style.backgroundColor = "#5cb85c";
+});
+
+document.getElementById("btn-secondary").addEventListener("click", function() {
+    document.getElementById("div-body").style.backgroundColor = "#9FA6B2";
+});
+
+document.getElementById("btn-warning").addEventListener("click", function() {
+    document.getElementById("div-body").style.backgroundColor = "#f0ad4e";
+});
+
+document.getElementById("btn-light").addEventListener("click", function() {
+    document.getElementById("div-body").style.backgroundColor = "#f7f7f7";
+});
+
+document.getElementById("btn-dark").addEventListener("click", function() {
+    document.getElementById("div-body").style.backgroundColor = "#292b2c";
+});
+
+
+    
