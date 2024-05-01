2cedd85278973d24469ccf4f4a6334d97345b443
Iam-Jam <jammietorayno@gail.com>
2024-05-01 17:38:59 +0800

    updates

---
+++ myreactapp/src/App.js
@@ -0,0 +1,14 @@
+// import "./../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import React from 'react';
+import Navbar from './ForMyPP4/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* Your other components and content */}
+    </div>
+  );
+}
+
+export default App;
